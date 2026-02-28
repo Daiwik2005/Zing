0fdc75f5e89bff698309eb89e1f88de7fddfce5e
Daiwik Palavarapu <Daiwikpalavarapu755@gmail.com>
2026-02-28 12:45:33 +0530

    working of github api is done

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,14 @@
-require("dotenv").config()
-console.log("TOKEN:", process.env.GITHUB_API);
+const handler = require("./agents/githubagent")
 const exp=require("express")
 const app=exp()
-const {Octokit} = require("octokit")
+const cors = require("cors")
+app.use(cors())
+
 
 // require the agent class; githubagent.js exports the class itself
-app.get("/repos/:username", async (req, res) => {
-    // grab the username from the URL parameter
-    const octo = new Octokit({
-        auth: process.env.GITHUB_API
-    })
-    
-    await octo.request("GET /users/{username}/repos", {
-        username: "daiwik2005"
-    }).then((response) => {
-        console.log(response.data);
-    }).catch((error) => {
-        console.error("Error fetching repositories:", error);
-    }
-    ) 
-})
-
+app.get("/repos/:username", handler)
 // start the server on port 3000 (or use env PORT)
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
