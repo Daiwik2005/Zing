7fd029a4bd058500560f8b000f068160f113c6ad
Daiwik Palavarapu <Daiwikpalavarapu755@gmail.com>
2026-04-29 22:46:25 +0530

    start

---
+++ backend/schemas.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+
+class UserCreate(BaseModel):
+    email: str
+    password: str
+
+#probably we can add further validation to this model, such as password strength, email format, etc.
+class UserLogin(BaseModel):
+    email: str
+    password: str
+
+    
